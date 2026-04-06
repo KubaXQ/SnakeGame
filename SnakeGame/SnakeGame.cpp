2773e5e7a6be9d128c28058788cd4dc4dbf77269
Jakub Mazanek <kubamazanek1@gmail.com>
2026-04-06 16:10:54 +0200

    Added state struct

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -2,12 +2,18 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 using namespace std;
+struct SDLState
+{
+	SDL_Window* window;
+	SDL_Renderer* render;
+};
+void cleanup(SDLState& state);
 
-void cleanup(SDL_Window* window);
+
 
 int main(int argc, char *argv[])
 {
-	
+	SDLState state;
 	if (!SDL_Init(SDL_INIT_VIDEO))
 	{
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "ERROR", "ERROR INITIALIZING SDL3",nullptr);
@@ -15,15 +21,22 @@ int main(int argc, char *argv[])
 	}
 	int width = 800;
 	int height = 400;
-	SDL_Window* window = SDL_CreateWindow("Game Window", width, height, 0);
+	state.window = SDL_CreateWindow("Game Window", width, height, 0);
 
-	if (!window)
+	if (!state.window)
 	{
-		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "ERROR", "Window not created", window);
-		cleanup(window);
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "ERROR", "Window not created", state.window);
+		cleanup(state);
 		return 1;
 	}
 	
+	//rendering
+
+	state.render = SDL_CreateRenderer(state.window, nullptr);
+
+
+
+	//Main loop
 	bool running = true;
 	while (running)
 	{
@@ -39,11 +52,13 @@ int main(int argc, char *argv[])
 	}
 
 
-	cleanup(window);
+	cleanup(state);
 	
 	return 0;
 }
-void cleanup(SDL_Window* window) {
-	SDL_DestroyWindow(window);
+void cleanup(SDLState &state) 
+{
+	SDL_DestroyRenderer(state.render);
+	SDL_DestroyWindow(state.window);
 	SDL_Quit();
 }
