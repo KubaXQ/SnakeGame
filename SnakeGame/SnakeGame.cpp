4e9bffaf94660f918a92089eb07325a7da570d3c
Jakub Mazanek <kubamazanek1@gmail.com>
2026-04-06 17:28:43 +0200

    Added new library SDL_image

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -1,6 +1,8 @@
 ﻿#include "SnakeGame.h"
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+
 using namespace std;
 struct SDLState
 {
@@ -33,7 +35,12 @@ int main(int argc, char *argv[])
 	//rendering
 
 	state.render = SDL_CreateRenderer(state.window, nullptr);
-
+	if (!state.render)
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "ERROR", "Error creating render", state.window);
+		cleanup(state);
+		return 1;
+	}
 
 
 	//Main loop
@@ -48,7 +55,10 @@ int main(int argc, char *argv[])
 			}
 		}
 
+		SDL_SetRenderDrawColor(state.render, 255, 2, 255,255);
+		SDL_RenderClear(state.render);
 
+		SDL_RenderPresent(state.render);
 	}
 
 
