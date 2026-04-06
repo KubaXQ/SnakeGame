5a909decc80490c4dd8d135253a0a164903f429f
Jakub Mazanek <kubamazanek1@gmail.com>
2026-04-06 15:01:05 +0200

    init project

---
+++ SnakeGame/SnakeGame.cpp
@@ -0,0 +1,13 @@
+﻿#include "SnakeGame.h"
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Quit();
+	
+	return 0;
+}
