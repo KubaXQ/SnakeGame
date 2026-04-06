59af1ece2ccbef2971ae9ed242cc17f918b9200e
Jakub Mazanek <kubamazanek1@gmail.com>
2026-04-06 15:34:57 +0200

    Added error handle for initializing SDL3 and Window

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -3,11 +3,32 @@
 #include <SDL3/SDL_main.h>
 using namespace std;
 
+void cleanup(SDL_Window* window);
+
 int main(int argc, char *argv[])
 {
-	SDL_Init(SDL_INIT_VIDEO);
+	
+	if (!SDL_Init(SDL_INIT_VIDEO))
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "ERROR", "ERROR INITIALIZING SDL3",nullptr);
+		return 1;
+	}
+	int width = 800;
+	int height = 400;
+	SDL_Window* window = SDL_CreateWindow("Game Window", width, height, 0);
 
-	SDL_Quit();
+	if (!window)
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "ERROR", "Window not created", window);
+		cleanup(window);
+		return 1;
+	}
+
+	cleanup(window);
 	
 	return 0;
+}
+void cleanup(SDL_Window* window) {
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
